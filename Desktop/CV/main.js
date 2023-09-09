b326ef037005b0f4140a81e13ecd6c94af5024ea
Mahmoud Eid <Mahmoudeid1840@gmail.com>
2023-09-09 15:44:37 +0300

    my portofilio

---
+++ Desktop/CV/main.js
@@ -0,0 +1,55 @@
+const selector=(ele)=>document.querySelector(ele);
+const selectorAll=(all)=>document.querySelectorAll(all);
+
+selector('.links').addEventListener('click',(e)=>{
+    selector('nav .links .icon span:nth-child(2)').classList.toggle('full')
+    selector('nav .contents').classList.toggle('open');
+})
+
+
+let skills=document.querySelector('.our-skills');
+let progress=Array.from(document.querySelectorAll('.prog span'));
+
+window.addEventListener('scroll',skillsScroll)
+    function skillsScroll(){
+        if(window.scrollY >= skills.offsetTop - 50){
+            progress.forEach((prog)=>{
+                prog.style.width=prog.dataset.progress;
+                prog.style.transition="2s ease-in-out";
+            })
+    }
+}
+
+let bulltesSkills=[...document.querySelectorAll('.bullets >li')];
+bulltesSkills.forEach((bullet)=>{
+  bullet.addEventListener(('click'),()=>{
+    bulltesSkills.forEach((bullet)=>{
+        bullet.style.backgroundColor="transparent"
+    })
+    bullet.style.backgroundColor=bullet.dataset.color;
+    progress.forEach((prog)=>{
+        prog.style.backgroundColor=bullet.dataset.color;
+    })
+  })
+})
+
+ window.onload=function(){
+    selector('.landing .image').style.right='0';
+    selector('.container .landing .info').style.opacity='1';
+
+ }
+
+ window.addEventListener('scroll',porto)
+ function porto(){
+     if(window.scrollY >= selector('.portofilio .container .projects ').offsetTop - 480){
+         selector('.portofilio .container .projects ').style.opacity="1"
+ }
+}
+
+let jump=document.querySelector('.up');
+    jump.addEventListener(('click'),()=>{
+        window.scrollTo({
+            top:0,
+            left:0,
+        })
+    })
